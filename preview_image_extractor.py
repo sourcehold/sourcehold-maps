de3ad4e45f4403392b308fedb94db1416f4747d8
Lawrence Foley <emwav333@gmail.com>
2019-09-18 14:41:44 -0500

    Rework preview_image_extractor. Relative .dll path for compressor.

--- preview_image_extractor.py
+++ preview_image_extractor.py
@@ -1,47 +1,73 @@
+import argparse
+import logging
+import os
+import io
+import sys
+from pathlib import PurePath
 
-import palette, io, subprocess
 from PIL import Image
 
-def extract_preview(f):
-    data = f.read()
+import maps
+import palette
+import structure_tools
 
-    result = subprocess.run("resources/blast.exe", input = data[20:], stdout = subprocess.PIPE)
-    result.check_returncode()
+palette_size = 512
 
-    f2 = io.BytesIO(result.stdout)
 
-    size = len(result.stdout)
-    image_size = size - 512
-    print(image_size)
-    width = int(image_size ** 0.5)
-    height = int(image_size ** 0.5)
+def extract_preview(data):
+    buff = io.BytesIO(data)
 
-    f2.seek(0)
-    p = palette.build_serial_palette(f2)
-    data = list(f2.read(image_size))
+    size = len(data)
+    image_size = size - palette_size
+    width = height = int(image_size ** 0.5)
 
-    image = Image.new("P", (width, height))
-    image.putpalette(p)
-    image.putdata(data)
+    buff.seek(0)
+    p = palette.build_serial_palette(buff)
+    data = list(buff.read(image_size))
 
-    return image
+    img = Image.new("P", (width, height))
+    img.putpalette(p)
+    img.putdata(data)
+
+    return img
 
 
 if __name__ == "__main__":
-    import sys
-    if len(sys.argv) == 1:
-        sys.argv.append("resources/MxM_unseen_1.map")
-        #raise Exception("Too few arguments. No inputfile and optional outputfile specified")
-    inputfile = outputfile = None
-    if len(sys.argv) >= 2:
-        inputfile = sys.argv[1]
-    if len(sys.argv) == 3:
-        outputfile = sys.argv[2]
+    logging.basicConfig(level=logging.DEBUG)
 
-    with open(inputfile, 'rb') as f:
-        image = extract_preview(f)
-        if outputfile:
-            with open(outputfile, 'wb') as f2:
-                image.save(f2)
-        else:
-            image.show()
+    # if len(sys.argv) == 1:
+    #     sys.argv.append("resources/MxM_unseen_1.map")
+    #     # raise Exception("Too few arguments. No inputfile and optional outputfile specified")
+    # inputfile = outputfile = None
+    # if len(sys.argv) >= 2:
+    #     inputfile = sys.argv[1]
+    # if len(sys.argv) == 3:
+    #     outputfile = sys.argv[2]
+
+    arg_parser = argparse.ArgumentParser(description="Extract the preview image from a Stronghold .map file.")
+    arg_parser.add_argument("-map", dest="map_file", action="store", metavar="MyMap.map",
+                            required=True,
+                            help="A Crusader .map file")
+    arg_parser.add_argument("-out", dest="out_file", action="store", metavar="preview.png",
+                            required=False,
+                            help="A filename to save the preview image")
+
+    args = arg_parser.parse_args()
+
+    map_file = args.map_file
+    out_file = args.out_file
+
+    if PurePath(map_file).suffix != ".map":
+        logging.error("Map file must end with '.map'")
+        exit(1)
+
+    if out_file is None:
+        logging.info("No output filename specified, using map name")
+        out_file = PurePath(map_file).stem + ".png"
+
+    with open(os.path.join(os.path.dirname(__file__), map_file), "rb") as f:
+        buf = structure_tools.Buffer(f.read())
+
+    m = maps.Map().from_buffer(buf)
+    image = extract_preview(m.preview.get_data())
+    image.save(out_file)
