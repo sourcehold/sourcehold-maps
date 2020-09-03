05fbe39cc6e988c1856aa54b153690d04b9cbc09
gynt <gynt@users.noreply.github.com>
2020-09-03 16:58:10 +0200

    change shell to cmd for windows-latest

--- compression/compile.bat
+++ compression/compile.bat
@@ -1,3 +1,3 @@
 
 cl.exe /c crc32.c explode.c implode.c buffers.cpp
-link.exe /DLL /DEF:compressionlib-nocb.def crc32.obj buffers.obj explode.obj implode.obj
+link.exe /DLL /DEF:compressionlib-nocb.def crc32.obj buffers.obj explode.obj implode.obj /OUT:compressionlib-nocb.dll
