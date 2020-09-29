cd4ce2ad5c9f5d3c067aed417c0db4f73a56dffb
gynt <gynt@users.noreply.github.com>
2020-09-30 01:00:58 +0200

    don't bother testing on x64 machines for x86 python anymore

--- examples/process_memory_live_manipulation.py
+++ examples/process_memory_live_manipulation.py
@@ -1,10 +1,17 @@
 ## Example:
 
 from sourcehold.debugtools.memory.common.access import AccessContext
-from sourcehold.debugtools.memory.common.manipulation import LiveSection1015
+from sourcehold.debugtools.memory.common.manipulation import LiveSection1015, LiveSection1013, LiveSection1022
 
+process = AccessContext()
+units = LiveSection1015(process)
+units.unpack_items()
 
-live = LiveSection1015(AccessContext())
-live.unpack_items()
-with live.cache():
-    print(live[0].location_tile_index)
+with units.cache():
+    print(units[1].location_tile_index)
+
+buildings = LiveSection1013(process)
+buildings.unpack_items()
+
+player_data = LiveSection1022(process)
+player_data.unpack_items()
--- sourcehold/maps/sections/objects/__init__.py
+++ sourcehold/maps/sections/objects/__init__.py
@@ -78,6 +78,39 @@ class PlayerData(ChildStructure):
 
     data = DataProperty("B", start=0, array_size=14836)
 
+    # Popularity level. Starting popularity is 10000.
+    popularity = DataProperty("H", start=96)
+
+
+    # 0: floor(inhabitants/2)*2 (factor -1)
+    # 1: inhabitants/72 * 57 (factor -0.8)
+    # 2: inhabitants/72 * 43 (factor -0.6)
+    # 3: 0                   (factor 0)
+    # 4: inhabitants/72 * 43 (factor 0.6)
+    # 5: inhabitants/72 * 57 (factor 0.8)
+    # 6: floor(inhabitants/2)*2 , or: inhabitants/72 * 72 (factor 1)
+    # 7: inhabitants/72 * 86 (factor 1.2)
+    # 8: inhabitants/72 * 100 (factor 1.4)
+    # 9: inhabitants/72 * 115 (factor 1.6)
+    # 10: inhabitants/72 * 129 (factor 1.8)
+    # 11: inhabitants/72 * 144 (factor 2)
+    # 12: factor 2.2
+    # 13: factor 2.4
+    # 14: factor 2.6
+    # 15: factor 2.8
+    # 16: factor 3
+    # 99: inhabitants/72 * 1411
+    # 100: inhabitants/72 * 1425
+    # 121: inhabitants/72 * 1728 (factor 24)
+    # 1331: inhabitants/72 * 19152 (factor 266)
+    # 9000: inhabitants/72 * 129585
+    # 14641: inhabitants/72 * 210816 (factor 2928)
+    # So, the factor increases by 0.2 for every 1 increase in tax setting.
+    #
+
+    # a value from 0 until 12, indicating the tax setting. default is 3. Setting it to 12 produces "army wages" with +121 popularity. 13 -> "Inn", 14 -> "Turn Off X"
+    tax_setting = DataProperty("B", start=8584)
+
     #unit_aiv_locations = DataProperty(UnitAivLocations, start=11588)
 
     def __init__(self, parent, offset):
@@ -98,7 +131,7 @@ class AivUnitType(IntEnum):
     BALLISTA = 3
     TREBUCHET = 4
     FIRE_BALLISTA = 5
-    BOWMAN = 6
+    ARCHER = 6
     CROSSBOWMAN = 7
     SPEARMAN = 8
     PIKEMAN = 9
